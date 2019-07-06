3f3155d2bab6203645c731117c6948509533f7c6
truthconductor <truthconductor@gmail.com>
2019-07-06 15:39:31 +0900

    Add association to models
    group, user has_many messages
    message belongs_to group, user

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,7 @@
 class Group < ApplicationRecord
   has_many :members
   has_many :users, through: :members
+  has_many :messages
 
   #name属性に値が存在しない場合バリデーションエラー
   validates :name, presence: true, uniqueness: true
