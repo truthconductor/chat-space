d13c317ac079823177654609ac5a0042d25a4399
truthconductor <truthconductor@gmail.com>
2019-07-06 13:48:03 +0900

    change root path
    change messages nested groups

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root to: 'groups#index'
 
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update, :index]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index]
+  end
 end
