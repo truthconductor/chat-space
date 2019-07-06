717278a21a9773f1d103336bb45b8908ae842163
truthconductor <truthconductor@gmail.com>
2019-07-06 11:54:19 +0900

    Add ChatGroup create
    changed create, new method.
    changed new.html.haml

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,13 +1,28 @@
 class GroupsController < ApplicationController
-  def create
+  def new
+    @group = Group.new
   end
 
-  def new
+  def create
+    #groupとuser_idsで関連付いたuserを合わせて作成
+    @group = Group.new(create_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+  end
+
+  private
+
+  #formで送られてきたパラメーターをストロングパラメータとして取得
+  def create_params
+    params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    
     if current_user.update(user_params)
       redirect_to controller: 'messages', action: 'index'
     else
@@ -13,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   private
-  
+
   def user_params
     params.require(:user).permit(:name, :email)
   end
