63a67085a2699eda06b444d2aebc2a4262373ce5
truthconductor <truthconductor@gmail.com>
2019-07-06 18:31:14 +0900

    Update message.rb
    set mount_uploader to message model.

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,6 +2,9 @@ class Message < ApplicationRecord
   belongs_to :group
   belongs_to :user
 
-  #imageを送らないときテキストは必ず入れるようにするバリデーション
+  # imageを送らないときテキストは必ず入れるようにするバリデーション
   validates :body, presence: true, unless: :image?
+
+  # アップローダーを紐付け
+  mount_uploader :image, ImageUploader
 end
