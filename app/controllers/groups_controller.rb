b7dbd682dfec625bd15bd2211c10b55bea15f77c
truthconductor <truthconductor@gmail.com>
2019-07-06 13:04:37 +0900

    Add group edit function
    Update groups edit, update function
    fix edit.html.haml %form to form_for method use

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,7 +5,7 @@ class GroupsController < ApplicationController
 
   def create
     #groupとuser_idsで関連付いたuserを合わせて作成
-    @group = Group.new(create_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to root_path, notice: 'グループを作成しました'
     else
@@ -14,15 +14,22 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを編集しました'
+    else
+      render :edit
+    end
   end
 
   private
 
   #formで送られてきたパラメーターをストロングパラメータとして取得
-  def create_params
+  def group_params
     params.require(:group).permit(:name, { :user_ids => [] })
   end
 end
