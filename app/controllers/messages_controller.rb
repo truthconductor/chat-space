69ade99947abbfcfb915d306a3e864c37e4f6223
truthconductor <truthconductor@gmail.com>
2019-07-07 23:26:54 +0900

    Update messages_controller.rb
    add before_action :set_group

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,14 +1,13 @@
 class MessagesController < ApplicationController
+  before_action :set_group, only: [:index, :create]
   def index
-    @group = Group.find(params[:group_id])
-    @message = Message.new()
+    @message = Message.new
   end
 
   def create
-    @message = Message.new(message_param)
+    @message = @group.messages.new(message_param)
     # バリデーション
     unless @message.valid?
-      @group = Group.find(params[:group_id])
       flash.now[:alert] = "メッセージを入力してください。"
       render :index
       return
@@ -16,9 +15,8 @@ class MessagesController < ApplicationController
 
     #メッセージ保存（送信）
     if @message.save
-      redirect_to controller: "messages", action:"index", id: params[:id], notice: "メッセージを送信しました"
+      redirect_to group_messages_path(@group), notice: "メッセージを送信しました"
     else
-      @group = Group.find(params[:group_id])
       flash.now[:alert] = "メッセージを送信できませんでした。"
       render :index
     end
@@ -26,7 +24,11 @@ class MessagesController < ApplicationController
 
   private
 
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
+
   def message_param
-    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 end
