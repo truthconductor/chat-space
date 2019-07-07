6d6f2784512dfb5c859c4cf488cd4edb19b27ca3
truthconductor <truthconductor@gmail.com>
2019-07-07 22:12:09 +0900

    Add flash message
    add message validation in create function
    add validation error style scss

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,6 +5,18 @@ class MessagesController < ApplicationController
   end
 
   def create
-    
+    @message = Message.new(message_param)
+    unless @message.valid?
+      @group = Group.find(params[:group_id])
+      flash.now[:alert] = "メッセージを入力してください。"
+      render :index
+      return
+    end
+  end
+
+  private
+
+  def message_param
+    params.require(:message).permit(:body, :image)
   end
 end
