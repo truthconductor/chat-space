08bd24529f447413f7203f5bbd292d7646f9787a
truthconductor <truthconductor@gmail.com>
2019-07-07 22:53:12 +0900

    Update messages_controller.rb
    add message strong parametter
    add message create funtion

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,27 @@ class MessagesController < ApplicationController
 
   def create
     @message = Message.new(message_param)
+    # バリデーション
     unless @message.valid?
       @group = Group.find(params[:group_id])
       flash.now[:alert] = "メッセージを入力してください。"
       render :index
       return
     end
+
+    #メッセージ保存（送信）
+    if @message.save
+      redirect_to controller: "messages", action:"index", id: params[:id], notice: "メッセージを送信しました"
+    else
+      @group = Group.find(params[:group_id])
+      flash.now[:alert] = "メッセージを送信できませんでした。"
+      render :index
+    end
   end
 
   private
 
   def message_param
-    params.require(:message).permit(:body, :image)
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
