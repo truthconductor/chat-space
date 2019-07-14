8089073c666bfe696bf428fedb466de52a76da9f
truthconductor <truthconductor@gmail.com>
2019-07-14 16:11:25 +0900

    Update messages.js
    add buildMesssage html
    append html to ".chat--messages"

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,4 +1,55 @@
 $(function() {
+  // 個々のmessageを表示するHTMLを作成
+  function buildMessage(message)
+  {
+    var html = null;
+    if(message.body && message.image)
+    {
+      html = `<div class="message">
+                <div class="message--title">
+                  <span class="message--title__post-user">${message.user_name}</span>
+                  <span class="message--title__post-date">${message.create_date}</span>
+                  <div class="message--text">
+                    ${message.body}
+                  </div>
+                  <div class="message--image">
+                    <img src="${message.image.url}">
+                  </div>
+                </div>
+              </div>`;
+    }
+    else if(message.body)
+    {
+      html = `<div class="message">
+                <div class="message--title">
+                  <span class="message--title__post-user">${message.user_name}</span>
+                  <span class="message--title__post-date">${message.create_date}</span>
+                  <div class="message--text">
+                    ${message.body}
+                  </div>
+                </div>
+              </div>`;
+    }
+    else if(message.image)
+    {
+      html = `<div class="message">
+                <div class="message--title">
+                  <span class="message--title__post-user">${message.user_name}</span>
+                  <span class="message--title__post-date">${message.create_date}</span>
+                  <div class="message--image">
+                    <img src="${message.image.url}">
+                  </div>
+                </div>
+              </div>`;
+    }
+
+    if(html)
+    {
+      // 作成したHTMLをメッセージ画面の一番下に追加する
+      $(".chat--messages").append(html);
+    }
+
+  }
   $(".new_message").on("submit", function(e) {
     e.preventDefault();
     var formData = new FormData(this);
@@ -10,6 +61,13 @@ $(function() {
       dataType: "json",
       processData: false,
       contentType: false
+    })
+    .done(function(message) {
+      buildMessage(message);
+    })
+    .fail(function()
+    {
+      alert("エラー");
     });
   });
 })
